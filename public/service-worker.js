51b45f658b254a95199650c1e2a1baf9a9046c47
Adam Keyser <65785770+adamkeyser45@users.noreply.github.com>
2020-10-09 15:04:59 -0500

    added more files to FILES_TO_CACHE

--- public/service-worker.js
+++ public/service-worker.js
@@ -3,7 +3,20 @@ const DATA_CACHE_NAME = 'data-cache-v1';
 
 const FILES_TO_CACHE = [
     '/',
-    '/index.html'
+    '/index.html',
+    '/manifest.json',
+    '/service-worker.js',
+    '/css/style.css',
+    '/icons/icon-72x72.png',
+    '/icons/icon-96x96.png',
+    '/icons/icon-128x128.png',
+    '/icons/icon-144x144.png',
+    '/icons/icon-152x152.png',
+    '/icons/icon-192x192.png',
+    '/icons/icon-384x384.png',
+    '/icons/icon-512x512.png',
+    '/js/idb.js',
+    '/js/index.js'
 ];
 
 self.addEventListener('install', function (evt) {
